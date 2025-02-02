595692b9d4d789cf814af23e9b8f09645196a798
Aiswarya-Kumar99 <aiswaryavijayakumar99@gmail.com>
2025-02-02 12:49:56 -0600

    character count - digital shredding

--- hw2.py
+++ hw2.py
@@ -1,6 +1,8 @@
 import sys
 import math
-
+import re
+from pathlib import Path
+from collections import Counter
 
 def get_parameter_vectors():
     '''
@@ -37,10 +39,13 @@ def get_parameter_vectors():
 def shred(filename):
     #Using a dictionary here. You may change this to any data structure of
     #your choice such as lists (X=[]) etc. for the assignment
-    X=dict()
+    X={chr(i):0 for i in range(ord('A'),ord('Z')+1)}
     with open (filename,encoding='utf-8') as f:
         # TODO: add your code here
-
+        content = f.read().upper()
+        content = re.sub(r'[^A-Z]','',content)
+        letterCount = Counter(content)
+        X.update(letterCount)
     return X
 
 
@@ -48,3 +53,14 @@ def shred(filename):
 # TODO: add your code here for the assignment
 # You are free to implement it as you wish!
 # Happy Coding!
+if len(sys.argv)<2:
+    print("Please enter the file to read")
+else:
+    filepath = sys.argv[1]
+    count = shred(filepath)
+    fileName = Path(filepath).stem
+    outputFile = f"{fileName}_out"
+    with open(outputFile,'w') as f:
+        f.write("Q1\n")
+        for char,freq in count.items():
+            f.write(f"{char} {freq}\n")
